1afcd1eaca00441de1bc8c286c8274650f422b97
mardt <itsmardt@gmail.com>
2024-03-11 17:11:49 +0100

    bat enemy moon enemy types

---
+++ BloomGame/Assets/Scripts/Enemy/Bat.cs
@@ -0,0 +1,20 @@
+using SmoothShakePro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : EnemyBase
+{
+    public Transform hitPosition;
+    public float attackRange = 5;
+    public AnimationClip attackClip;
+    public override float AttackDuration() => attackClip.length;
+    public override void DealDamage()
+    {
+        if (Vector3.Distance(player.position, hitPosition.position) < attackRange)
+        {
+            Player.instance.health--;
+            Debug.Log("Player health is " + Player.instance.health);
+        }
+    }
+}
